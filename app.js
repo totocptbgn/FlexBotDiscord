17d0b5809f182c29a74a22535256c1ecd9b390ff
Thomas Copt-Bignon <35891830+totocptbgn@users.noreply.github.com>
2018-09-22 23:36:52 +0200

    Update and rename bot.js to app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { Client, RichEmbed } = require('discord.js');
+
+client.on('ready', () => {
+  console.log("Done.");
+});
+
+client.on('message', message => {
+  var prefix = ".";
+
+  // ABOUT
+  if (message.content === prefix + 'about') {
+    const embed = new RichEmbed()
+      .setTitle('About :')
+      .setColor(0x61039A)
+      .setTimestamp()
+      .setDescription("I'm a bot created by totoLeFlex#9103.\n Use the prefix '.' to use me.\n List of the command :\n - .about\n - .hi\n - .tg");
+    message.channel.send(embed);
+    message.delete()
+      .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+      .catch(console.error);
+  }
+
+  //  HI
+  if (message.content === prefix + 'hi') {
+    message.channel.send("Hi, " + message.author.toString() + ".");
+  }
+
+  // TG
+  if (message.content === prefix + 'tg') {
+    message.delete()
+      .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+      .catch(console.error);
+    message.channel.send("Ferme ta gueule ! :joy:");
+  }
+
+});
+
+client.login(process.env.BOT_TOKEN);
