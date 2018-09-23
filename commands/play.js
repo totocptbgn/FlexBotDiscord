3353a000aef0633d49736973f1fe5670458ce284
Thomas Cpt-Bgn <35891830+totocptbgn@users.noreply.github.com>
2018-09-23 17:10:37 +0200

    play.js

--- commands/play.js
+++ commands/play.js
@@ -1,14 +1,19 @@
 const ytdl = require('ytdl-core');
 
 exports.run = async (client, message, args, ops) => {
-  // MUSIC
   if (!message.member.voiceChannel) return message.channel.send("Sorry " + message.author.toString() + ", but your are not in a voice channel :confused:");
+
   if (message.guild.me.voiceChannel) return message.channel.send("Sorry " + message.author.toString() + ", I'm already connected to the voice channel ! :thinking:");
+
   if (!args[0]) return message.channel.send("Sorry " + message.author.toString() + ", please put a valid URL ! :thinking:");
+
   let validate = await ytdl.validateURL(args[0]);
   if (!validate) return message.channel.send("Sorry " + message.author.toString() + ", please put a valid URL ! :thinking:");
+
   let info = await ytdl.getInfo(args[0]);
+
   let connection = await message.member.voiceChannel.join();
+
   let dispatcher = await connection.play(ytdl(args[0], { filter: 'audioonly'}));
   message.channel.send(`Now playing: ${info.title}`);
 }
