fe86a35723b27028fbca182b3d15967b679984f8
Thomas Cpt-Bgn <35891830+totocptbgn@users.noreply.github.com>
2018-09-23 16:05:43 +0200

    Music added

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
 const client = new Discord.Client();
 const { Client, RichEmbed } = require('discord.js');
 
@@ -54,5 +55,18 @@ client.on('message', message => {
     .catch(console.error);
   }
 });
+
+exports.run = async (client, message, args, ops) => {
+  // MUSIC
+  if (!message.author.voiceChannel) return message.channel.send("Sorry " + message.author.toString() + ", but your are not in a voice channel :confused:");
+  if (!message.guild.me.voiceChannel) return message.channel.send("Sorry " + message.author.toString() + ", I'm already connected to the voice channel ! :thinking:");
+  if (!args[0]) return message.channel.send("Sorry " + message.author.toString() + ", please put a valid URL ! :thinking:");
+  let validate = await ytdl.validateURL(args[0]);
+  if (!validate) return message.channel.send("Sorry " + message.author.toString() + ", please put a valid URL ! :thinking:");
+  let info = await ytdl.getInfo(args[0]);
+  let connection = await message.member.voiceChannel.join();
+  let dispatcher = await connection.play(ytdl(args[0], { filter: 'audioonly'}));
+  message.channel.send(`Now playing: ${info.title}`);
+}
 
 client.login(process.env.BOT_TOKEN);
