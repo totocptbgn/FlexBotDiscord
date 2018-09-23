ba0e7f27f24ea3551f5a8d0c7540c3a265059ea9
Thomas Cpt-Bgn <35891830+totocptbgn@users.noreply.github.com>
2018-09-23 16:29:24 +0200

    play.js

--- bot.js
+++ bot.js
@@ -54,19 +54,16 @@ client.on('message', message => {
     .then(msg => console.log(`Replied hi to ${msg.author.username} in ${msg.guild.name}`))
     .catch(console.error);
   }
+
+  // MUSIC
+  let args = message.content.slice(prefix.length).trim().split(' ');
+  let cmd = args.shift().toLowerCase();
+
+  if (!message.content.startsWith(prefix)) return;
+
+  let commandFile = require(`./${cmd}.js`)
+  commandFile.run(client, message, args);
 });
 
-exports.run = async (client, message, args, ops) => {
-  // MUSIC
-  if (!message.author.voiceChannel) return message.channel.send("Sorry " + message.author.toString() + ", but your are not in a voice channel :confused:");
-  if (!message.guild.me.voiceChannel) return message.channel.send("Sorry " + message.author.toString() + ", I'm already connected to the voice channel ! :thinking:");
-  if (!args[0]) return message.channel.send("Sorry " + message.author.toString() + ", please put a valid URL ! :thinking:");
-  let validate = await ytdl.validateURL(args[0]);
-  if (!validate) return message.channel.send("Sorry " + message.author.toString() + ", please put a valid URL ! :thinking:");
-  let info = await ytdl.getInfo(args[0]);
-  let connection = await message.member.voiceChannel.join();
-  let dispatcher = await connection.play(ytdl(args[0], { filter: 'audioonly'}));
-  message.channel.send(`Now playing: ${info.title}`);
-}
 
 client.login(process.env.BOT_TOKEN);
